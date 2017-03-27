64d04a0d79f69665b67781bed1bb42d55d197876
hinkmond <hibwong@ucsc.edu>
2017-03-26 21:51:59 -0700

    Refactored Thread t* names.

--- Java8Features/src/JavaLambdaExpr.java
+++ Java8Features/src/JavaLambdaExpr.java
@@ -3,26 +3,26 @@ public class JavaLambdaExpr {
   public static void main(String args[]) {
 
     // new Thread with anonymous inner class
-    Thread t1 = new Thread() {
+    Thread t0 = new Thread() {
+      public void run() {
+        System.out.println("Thread 0 started.");
+      }
+    };
+    t0.start();
+
+    // new Thread with anonymous class as a parameter
+    Thread t1 = new Thread(new Runnable() {
+      @Override
       public void run() {
         System.out.println("Thread 1 started.");
       }
-    };
+    });
     t1.start();
 
-    // new Thread with anonymous class as a parameter
-    Thread t2 = new Thread(new Runnable() {
-      @Override
-      public void run() {
-        System.out.println("Thread 2 started.");
-      }
-    });
-    t2.start();
-
     // Declare a Lambda Runnable
-    Runnable t3Runnable = () -> { System.out.println("Thread 3 started."); };
-    // new Thread with Lambda Expression
-    new Thread(t3Runnable).start();
+    Runnable t2Runnable = () -> { System.out.println("Thread 2 started."); };
+    // new Thread using Lambda Expression
+    new Thread(t2Runnable).start();
 
   }
 
