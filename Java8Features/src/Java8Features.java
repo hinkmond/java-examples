72f025aadd43996f3d74b1706991f42db0e75163
Hinkmond Wong <hiwong@linkedin.com>
2017-03-27 10:17:10 -0700

    Cleaned up indents and unneeded inner classes.

--- Java8Features/src/Java8Features.java
+++ Java8Features/src/Java8Features.java
@@ -5,47 +5,47 @@ import java.util.List;
 
 public class Java8Features {
 
-    static List<Integer> myList =
-        new ArrayList<>(Arrays.asList( 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ));
+  static List<Integer> myList =
+      new ArrayList<>(Arrays.asList( 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ));
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        // StringBuilder: Builder Design Pattern
-        StringBuilder sb = new StringBuilder();
-        String result = sb.append("do this ")
-            .append("do that ")
-            .append("do the other thing.")
-            .toString();
-        System.out.println("StringBuilder result: " + result);
+    // StringBuilder: Builder Design Pattern
+    StringBuilder sb = new StringBuilder();
+    String result = sb.append("do this ")
+        .append("do that ")
+        .append("do the other thing.")
+        .toString();
+    System.out.println("StringBuilder result: " + result);
 
-        // Typical for-loop
-        System.out.print("for-loop: ");
-        int numArray[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-        for (int i = 0; i<numArray.length; i++) {
-            System.out.print(numArray[i] + " ");
-        }
-        System.out.println();  // Line-feed
-
-        // Traversing using Iterator
-        System.out.print("Iterator: ");
-        Iterator<Integer> myListIterator = myList.iterator();
-        while (myListIterator.hasNext()) {
-            Integer i = myListIterator.next();
-            System.out.print(i + " ");
-        }
-        System.out.println();  // Line-feed
-
-        // Traversing using Generics and Enhanced for-loop
-        System.out.print("Generics & Enhanced for-loop: ");
-        for (Integer intVal : myList) {
-            System.out.print(intVal + " ");
-        }
-        System.out.println();  // Line-feed
-
-        // Traversing using forEach() and Lambdas
-        System.out.print("forEach() & Lambda Expression: ");
-        myList.forEach(intVal -> System.out.print(intVal + " "));
-        System.out.println();  // Line-feed
-
+    // Typical for-loop
+    System.out.print("for-loop: ");
+    int numArray[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    for (int i = 0; i<numArray.length; i++) {
+      System.out.print(numArray[i] + " ");
     }
+    System.out.println();  // Line-feed
+
+    // Traversing using Iterator
+    System.out.print("Iterator: ");
+    Iterator<Integer> myListIterator = myList.iterator();
+    while (myListIterator.hasNext()) {
+      Integer i = myListIterator.next();
+      System.out.print(i + " ");
+    }
+    System.out.println();  // Line-feed
+
+    // Traversing using Generics and Enhanced for-loop
+    System.out.print("Generics & Enhanced for-loop: ");
+    for (Integer intVal : myList) {
+      System.out.print(intVal + " ");
+    }
+    System.out.println();  // Line-feed
+
+    // Traversing using forEach() and Lambdas
+    System.out.print("forEach() & Lambda Expression: ");
+    myList.forEach(intVal -> System.out.print(intVal + " "));
+    System.out.println();  // Line-feed
+
+  }
 }
