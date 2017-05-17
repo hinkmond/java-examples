28c596ededb0c9daee0db526cabcb671244ce850
Hinkmond Wong <hiwong@linkedin.com>
2017-05-16 17:03:23 -0700

    New Data Streams example.

---
+++ Java8DataStreams/src/JavaAdvDataStreams01.java
@@ -0,0 +1,32 @@
+import javax.swing.text.html.Option;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class JavaAdvDataStreams01 {
+  public static void main(String[] args) {
+    String[] myArray = new String[] {
+        "Fred", "Debbie", "Theodore", "Lenny", "Justine"
+    };
+
+    // Create some sample Streams all from the same array
+    Stream<String> mapStream = Arrays.stream(myArray);
+
+    // Example of multiple maps
+    String newString = mapStream
+            .map(s -> s.replace("e", "3"))
+            .map(s -> s.replace("o", "0"))
+            .map(s -> s.replace("i", "1"))
+            .reduce("Reduction String", (a, s) -> a + ", " +
+                    s.toUpperCase());
+
+    // List all newly mapped elements
+    System.out.println(newString);
+
+    // Show result of convenient map count()
+    System.out.println("Number of Elements Using count() = ");
+  }
+}
