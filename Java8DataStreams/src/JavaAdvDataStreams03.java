62c86320d175175c1a2e9d293676516b4fe6c263
hinkmond <hibwong@ucsc.edu>
2017-08-22 22:39:43 -0700

    Updated to use ArrayList and HashMap.

--- Java8DataStreams/src/JavaAdvDataStreams03.java
+++ Java8DataStreams/src/JavaAdvDataStreams03.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -8,9 +6,9 @@ public class JavaAdvDataStreams03 {
 
   public static void main(String[] args) {
 
-    final List<String> femaleNamesList = Arrays.asList(
+    final List<String> femaleNamesList = new ArrayList<>(Arrays.asList(
         "Amy", "Ann", "Becky", "Debbie", "Jane", "Justine"
-    );
+    ));
 
     String[] myArray01 = new String[] {
         "FREd", "debbIe", "AMY", "JanE"
@@ -23,14 +21,14 @@ public class JavaAdvDataStreams03 {
     };
 
     // Create a sample Map with multiple Sets in each Entry
-    HashSet<String[]> namesMap = new HashSet<>();
-    namesMap.add(myArray01);
-    namesMap.add(myArray02);
-    namesMap.add(myArray03);
+    HashMap<Integer, List<String>> namesMap = new HashMap<>();
+    namesMap.put(1, new ArrayList<>(Arrays.asList(myArray01)));
+    namesMap.put(2, new ArrayList<>(Arrays.asList(myArray02)));
+    namesMap.put(3, new ArrayList<>(Arrays.asList(myArray03)));
 
     // Example of map, filter, then collect
-    List<String> foundNamesList = namesMap.stream()
-        .flatMap(s -> Arrays.stream(s).map(strElem -> {
+    List<String> foundNamesList = namesMap.values().stream()
+        .flatMap(mapValuesStream -> mapValuesStream.stream().map(strElem -> {
           String lowerCaseStr = strElem.toLowerCase();
           return Character.toUpperCase(lowerCaseStr.charAt(0)) +
               lowerCaseStr.substring(1);}))
