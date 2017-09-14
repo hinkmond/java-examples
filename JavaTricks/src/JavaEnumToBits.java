660976646657164c312e4433597c1b1246768c0d
hinkmond <hibwong@ucsc.edu>
2017-09-13 21:00:45 -0700

    Added way to use bit operators << and >> for setting and testing bits on long value.

--- JavaTricks/src/JavaEnumToBits.java
+++ JavaTricks/src/JavaEnumToBits.java
@@ -22,13 +22,28 @@ public class JavaEnumToBits {
     testInteger = testInteger.setBit(orderedEnum.FIVE.ordinal());
     testInteger = testInteger.setBit(orderedEnum.TWO.ordinal());
     testInteger = testInteger.setBit(orderedEnum.EIGHT.ordinal());
+    testInteger = testInteger.setBit(orderedEnum.THREE.ordinal());
 
+    testBits |= 1 << orderedEnum.ONE.ordinal();
+    testBits |= 1 << orderedEnum.FIVE.ordinal();
+    testBits |= 1 << orderedEnum.TWO.ordinal();
+    testBits |= 1 << orderedEnum.EIGHT.ordinal();
+    testBits |= 1 << orderedEnum.THREE.ordinal();
 
     System.out.println("JavaEnumToBits: testInteger = " + testInteger);
 
+    int counter = 1;
     for (orderedEnum val : orderedEnum.values()) {
-      System.out.println("JavaEnumToBits: testBit(val) = " +
+      System.out.println("JavaEnumToBits: testBit(val) " + counter++ + " = " +
           testInteger.testBit(val.ordinal()));
+    }
+
+    System.out.println("=====");
+
+    int bitIndex = 1;
+    for (orderedEnum index : orderedEnum.values()) {
+      System.out.println("java long to bits: testBit(val) = " + bitIndex++ + " = " +
+          (((testBits >> index.ordinal()) & 1) != 0));
     }
 
   }
